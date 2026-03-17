083e6ee7e563afabc539a1eb2d28cb3e353193df
michal92299 <chudybyczek2137@gmail.com>
2026-03-17 20:51:59 +0100

    Create helpers.go

---
+++ source-code/src/helpers.go
@@ -0,0 +1,95 @@
+package src
+
+import (
+	"unicode"
+	"unicode/utf8"
+
+	"github.com/mattn/go-runewidth"
+)
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func visualWidth(r rune, col int) int {
+	if r == '\t' {
+		return tabWidth - (col % tabWidth)
+	}
+	return runewidth.RuneWidth(r)
+}
+
+func visualCol(line string, bytePos int) int {
+	col := 0
+	j := 0
+	for j < bytePos {
+		r, size := utf8.DecodeRuneInString(line[j:])
+		if size == 0 {
+			break
+		}
+		col += visualWidth(r, col)
+		j += size
+	}
+	return col
+}
+
+func bytePosFromVisual(line string, target int) int {
+	col := 0
+	j := 0
+	for j < len(line) {
+		r, size := utf8.DecodeRuneInString(line[j:])
+		if size == 0 {
+			break
+		}
+		w := visualWidth(r, col)
+		if col+w > target {
+			break
+		}
+		col += w
+		j += size
+	}
+	return j
+}
+
+func utf8Prev(line string, pos int) int {
+	if pos <= 0 {
+		return 0
+	}
+	pos--
+	for pos > 0 && !utf8.RuneStart(line[pos]) {
+		pos--
+	}
+	return pos
+}
+
+func utf8Next(line string, pos int) int {
+	if pos >= len(line) {
+		return pos
+	}
+	_, size := utf8.DecodeRuneInString(line[pos:])
+	if size == 0 {
+		return len(line)
+	}
+	return pos + size
+}
+
+func leadingWhitespace(s string) string {
+	i := 0
+	for i < len(s) {
+		r, size := utf8.DecodeRuneInString(s[i:])
+		if !unicode.IsSpace(r) {
+			break
+		}
+		i += size
+	}
+	return s[:i]
+}
